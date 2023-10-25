0959174cac03e52fddc676e6b646cc308517902c
YYJ <193092379@qq.com>
2023-10-25 09:47:59 +0800

    交付第1版

--- try/code.js
+++ try/code.js
@@ -3,18 +3,15 @@ var svg = d3.select("svg"),
     width = +svg.node().getBoundingClientRect().width,
     height = +svg.node().getBoundingClientRect().height;
 
-// svg objects
 var link, node;
-// the data - an object with nodes and links
 var graph;
 var hasLink = new Set();
 
-// load the data
+
 d3.json("../data/sample6.json").then(_graph => {
     graph = _graph;
-    let dataset = graph,
-        getByNodeId = new Map();
-    dataset.as_groups.forEach(as => {
+    let getByNodeId = new Map();
+    graph.as_groups.forEach(as => {
         if (as.nodes) {
             as.nodes.forEach(node => {
                 getByNodeId.set(node.id, as.as_id)
@@ -26,9 +23,9 @@ d3.json("../data/sample6.json").then(_graph => {
     // 将每个边对象转换为d3.js所需的格式            
     let edgesData = [],
         degrees = {};
-    dataset.edges.forEach(function (edge) {
-        let s = getByNodeId.get(edge.source),
-            t = getByNodeId.get(edge.target)
+    graph.edges.forEach(function (edge) {
+        let s = getByNodeId.get(edge.source_id),
+            t = getByNodeId.get(edge.target_id)
         let key = s + "," + t
         if (!hasLink.has(key)) {
             if (s !== t) {
@@ -42,10 +39,10 @@ d3.json("../data/sample6.json").then(_graph => {
             hasLink.add(key)
         }
     });
-    const sortedNodes = Object.entries(degrees)
-        .sort((a, b) => b[1] - a[1]);
+    // const sortedNodes = Object.entries(degrees)
+    //     .sort((a, b) => b[1] - a[1]);
 
-    console.log(sortedNodes);
+    // console.log(sortedNodes);
     graph.nodes = graph.as_groups
     graph.links = edgesData
     graph.nodes.forEach(node => {
@@ -55,51 +52,6 @@ d3.json("../data/sample6.json").then(_graph => {
     initializeDisplay();
     initializeSimulation();
 });
-// d3.json("../data/sample6.json").then(_graph => {
-//     graph = _graph;
-//     let dataset = graph,
-//         getByNodeId = new Map();
-//     dataset.as_groups.forEach(as => {
-//         if (as.nodes) {
-//             as.nodes.forEach(node => {
-//                 getByNodeId.set(node.id, as.as_id)
-//             })
-//         }
-//     })
-//     console.log(getByNodeId);
-
-//     // 将每个边对象转换为d3.js所需的格式            
-//     let edgesData = [],
-//         degrees = {};
-//     dataset.edges.forEach(function (edge) {
-//         let s = getByNodeId.get(edge.source),
-//             t = getByNodeId.get(edge.target)
-//         let key = s + "," + t
-//         if (!hasLink.has(key)) {
-//             if (s !== t) {
-//                 edgesData.push({
-//                     source: s,
-//                     target: t
-//                 })
-//                 degrees[s] = (degrees[s] || 0) + 1;
-//                 degrees[t] = (degrees[t] || 0) + 1;
-//             }
-//             hasLink.add(key)
-//         }
-//     });
-//     const sortedNodes = Object.entries(degrees)
-//         .sort((a, b) => b[1] - a[1]);
-
-//     console.log(sortedNodes);
-//     graph.nodes = graph.as_groups
-//     graph.links = edgesData
-//     graph.nodes.forEach(node => {
-//         node.degree = degrees[node.as_id]
-//     })
-//     console.log(graph);
-//     initializeDisplay();
-//     initializeSimulation();
-// });
 
 
 
@@ -113,13 +65,13 @@ forceProperties = {
     },
     charge: {
         enabled: true,
-        strength: -8,
+        strength: -20,
         distanceMin: 500,
         distanceMax: 800
     },
     collide: {
         enabled: true,
-        strength: .1,
+        strength: .2,
         iterations: 1,
         radius: 8
     },
@@ -135,7 +87,9 @@ forceProperties = {
     },
     link: {
         enabled: true,
-        distance: 40,
+        degree_marker: 50,
+        distance1: 50,
+        distance2: 30,
         iterations: 1
     }
 }
@@ -146,56 +100,25 @@ var simulation = d3.forceSimulation();
 // 初始化力仿真
 function initializeSimulation() {
     simulation.nodes(graph.nodes)
-        .force("charge", d3.forceManyBody().strength(-20))
-        // .force("link", d3.forceLink(graph.links).id(d => d.name).distance(40))
-        .force("collide", d3.forceCollide().radius(8).strength(.2))
-        .force("center", d3.forceCenter().x(width / 3).y(height / 2))
-        .force("x", d3.forceX(width / 2).strength(forceProperties.forceX.strength))
-        .force("y", d3.forceY(height / 2).strength(forceProperties.forceX.strength))
+        .force("charge", d3.forceManyBody().strength(forceProperties.charge.strength))
+        .force("collide", d3.forceCollide().radius(forceProperties.collide.radius).strength(forceProperties.collide.strength))
+        .force("center", d3.forceCenter()
+            .x(width * forceProperties.center.x)
+            .y(height * forceProperties.center.x))
+        .force("x", d3.forceX(width * forceProperties.forceX.x).strength(forceProperties.forceX.strength))
+        .force("y", d3.forceY(height * forceProperties.forceY.y).strength(forceProperties.forceX.strength))
         .force("link", d3.forceLink(graph.links).id(d => d.as_id)
             .distance(d => {
-                if (d.target.degree > 50 || d.source.degree > 50)
-                    return 50
+                if (d.target.degree > forceProperties.link.degree_marker || d.source.degree > forceProperties.link.degree_marker)
+                    return forceProperties.link.distance1
                 else
-                    return 30
-            }))
+                    return forceProperties.link.distance2
+            })
+        );
 
     // simulation.alpha(0.7)
     simulation.velocityDecay(0.6);
-
-    // updateForces();
     simulation.on("tick", ticked);
-}
-
-// apply new force properties
-function updateForces() {
-    // get each force by name and update the properties
-    simulation.force("center")
-        .x(width * forceProperties.center.x)
-        .y(height * forceProperties.center.y);
-    simulation.force("charge")
-        .strength(forceProperties.charge.strength * forceProperties.charge.enabled)
-        .distanceMin(forceProperties.charge.distanceMin)
-        .distanceMax(forceProperties.charge.distanceMax);
-    simulation.force("collide")
-        .strength(forceProperties.collide.strength * forceProperties.collide.enabled)
-        .radius(forceProperties.collide.radius)
-        .iterations(forceProperties.collide.iterations);
-    simulation.force("forceX")
-        .strength(forceProperties.forceX.strength * forceProperties.forceX.enabled)
-        .x(width / 2);
-    simulation.force("forceY")
-        .strength(forceProperties.forceY.strength * forceProperties.forceY.enabled)
-        .y(height / 2);
-    // simulation.force("link")
-    //     .id(function (d) { return d.as_id; })
-    //     .distance(forceProperties.link.distance)
-    //     .iterations(forceProperties.link.iterations)
-    //     .links(forceProperties.link.enabled ? graph.links : []);
-
-    // updates ignored until this is run
-    // restarts the simulation (important if simulation has already slowed down)
-    // simulation.alpha(1).restart();
 }
 
 
@@ -234,7 +157,7 @@ function initializeDisplay() {
 
     node.append("title")
         .text(function (d) { return d.as_id; });
- 
+
 }
 
 
@@ -257,27 +180,25 @@ function highlightNode() {
         selectedId = selectedNode.select('circle').datum().as_id;
     // 再次点击，取消高亮，隐藏标签
     if (selectedNode.classed("selected")) {
-        d3.select('#show').classed("hidden", true)        
+        // d3.select('#show').classed("hidden", true)
         selectedNode.classed("selected", false);
-        
+
         // link.classed("link-highlight", false)
         // link.classed("link-blur", false)
     } else {
-        d3.select('#show').classed("hidden", false)
+        // d3.select('#show').classed("hidden", false)
         node.classed("selected", false)
         d3.select(this).classed("selected", true);
-                
+
         // 设置标签内容  
-        let no = selectedNode.select('circle').datum()        
+        let no = selectedNode.select('circle').datum()
         d3.select('#info h3').text(no.as_name);
         d3.select('#info #asId').text(selectedId);
         d3.select('#info #innerNodes').text(no.nodes.length);
         d3.select('#info #inDegree').text(no.degree);
     }
-
-
-
 }
+
 function dragstarted(event, d) {
     if (!event.active) simulation.alphaTarget(0.1).restart();
     d.fx = d.x;
